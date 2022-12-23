8fbe55f0357528c1e07fc9c224ba35e23d57840e
Mihail Rkitcky <papi.pay@mail.ru>
2022-12-23 12:36:55 +0300

    Задача №23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//5 ->
+//1 | 1
+//2 | 8
+//3 | 27
+//4 | 64
+//5 | 125
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+SquareTable(number);
+
+void SquareTable(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine($"{i} | {Math.Pow(i, 3)}");
+    }
+}
