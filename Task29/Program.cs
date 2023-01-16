aa002dafc99536f7406dccf76569d3d3858bff82
Mihail Rkitcky <papi.pay@mail.ru>
2023-01-16 23:46:52 +0300

    Задача №29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -2,13 +2,15 @@
 
 //1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
-Console.WriteLine("Введите длинну массива: ");
-int lengthArray = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Создаем массив длиной A в диапозоне от 0 до A");
+Console.Write("Введите любое число (A): ");
+int enterNumber = Convert.ToInt32(Console.ReadLine());
 
+int[] array = new int[enterNumber];
 
-int[] randomArray = new int[lengthArray];
-for (int i = 0; i < randomArray.Length; i++)
+for (int i = 0; i < enterNumber; i++)
 {
-    randomArray[i] = new Random().Next(1, 9);
-    Console.Write(randomArray[i] + " ");
+    array[i] = new Random().Next(enterNumber + 1);
+    Console.Write(array[i] + ", ");
 }
+Console.WriteLine();
