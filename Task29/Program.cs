a9f78803c91093dec2821a99528f96ca6159836d
Mihail Rkitcky <papi.pay@mail.ru>
2023-01-16 23:39:41 +0300

    Задача №29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -5,11 +5,10 @@
 Console.WriteLine("Введите длинну массива: ");
 int lengthArray = Convert.ToInt32(Console.ReadLine());
 
+
+int[] randomArray = new int[lengthArray];
+for (int i = 0; i < randomArray.Length; i++)
 {
-    int[] randomArray = new int[lengthArray];
-    for (int i = 0; i < randomArray.Length; i++)
-    {
-        randomArray[i] = new Random().Next(1, 9);
-        Console.Write(randomArray[i] + " ");
-    }
+    randomArray[i] = new Random().Next(1, 9);
+    Console.Write(randomArray[i] + " ");
 }
