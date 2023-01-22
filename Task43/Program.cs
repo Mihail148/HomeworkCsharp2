248f1395c386ed0ac54af0c2acb899e94568f83a
Mihail Rkitcky <papi.pay@mail.ru>
2023-01-22 20:25:51 +0300

    Задача №43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -12,9 +12,13 @@ Console.Write("Введите k2: ");
 double k2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите b2: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
- 
- 
+
+
 double x = -(b1 - b2) / (k1 - k2);
 double y = k1 * x + b1;
- 
+
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+
 Console.WriteLine($"Пересечение в точке: ({x};{y})");
