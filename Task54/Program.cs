01ce9b2e20505ca368adecbe81e2aaf77dbacc3e
Mihail Rkitcky <papi.pay@mail.ru>
2023-01-31 19:58:31 +0300

    Задача №54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -18,43 +18,43 @@ Console.WriteLine();
 PrintMatrix(matrix);
 
 
-void CreateMatrixRndNumbers(int[,] array)
+void CreateMatrixRndNumbers(int[,] matrix)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(1, 10);
+            matrix[i, j] = new Random().Next(1, 10);
         }
     }
 }
 
-void SortToLower(int[,] array)
+void SortToLower(int[,] matrix)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
             {
-                if (array[i, k] < array[i, k + 1])
+                if (matrix[i, k] < matrix[i, k + 1])
                 {
-                    int temp = array[i, k + 1];
-                    array[i, k + 1] = array[i, k];
-                    array[i, k] = temp;
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
                 }
             }
         }
     }
 }
 
-void PrintMatrix(int[,] array)
+void PrintMatrix(int[,] matrix)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{array[i, j]} ");
+            Console.Write($"{matrix[i, j]} ");
         }
         Console.WriteLine();
     }
